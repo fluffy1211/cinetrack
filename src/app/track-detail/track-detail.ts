19bfb54008237ebc349aa71be9debd461c6fcf87
Gabriel Martin <gabrielmartin13009@gmail.com>
2026-06-11 15:33:18 +0200

    feat: implement track detail page with delete action

--- src/app/track-detail/track-detail.ts
+++ src/app/track-detail/track-detail.ts
@@ -1,19 +1,25 @@
-// track-detail.ts
-import { Component, inject, input } from '@angular/core';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { switchMap } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { TrackService } from '../services/track.service';
 
 @Component({
   selector: 'app-track-detail',
+  imports: [RouterLink],
   templateUrl: './track-detail.html',
   styleUrl: './track-detail.css',
 })
 export class TrackDetail {
-  trackId = input.required<number>();
-  private service = inject(TrackService);
+  private trackService = inject(TrackService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
-  protected track = toSignal(
-    toObservable(this.trackId).pipe(switchMap((id) => this.service.getTrack(id))),
-  );
+  private id = Number(this.route.snapshot.paramMap.get('id'));
+  track = toSignal(this.trackService.getTrack(this.id));
+
+  protected delete() {
+    this.trackService.remove(this.id).subscribe(() => {
+      this.router.navigate(['/tracks']);
+    });
+  }
 }
